05c3268aa9eff885c6d00da751ad005cf7edd271
Marvin Dawson <marvintdawson@gmail.com>
2019-07-24 11:34:26 -0500

    updating IDE and enabled annotation in settings

--- core/src/main/java/com/wholaundry/Main.java
+++ core/src/main/java/com/wholaundry/Main.java
@@ -1,9 +1,11 @@
 package com.wholaundry;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@Slf4j
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
