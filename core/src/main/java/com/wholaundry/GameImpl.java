96c74e34b55f9ec937cf42f53894be7a858e79ce
Marvin Dawson <marvintdawson@gmail.com>
2019-08-04 20:18:09 -0500

    updating application

--- core/src/main/java/com/wholaundry/GameImpl.java
+++ core/src/main/java/com/wholaundry/GameImpl.java
@@ -3,6 +3,9 @@ package com.wholaundry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class GameImpl implements Game {
 
     // == constants ==
@@ -18,11 +21,8 @@ public class GameImpl implements Game {
     private int remainingGuesses;
     private boolean validNumberRange = true;
 
-   // == public methods ==
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }
-
+    // == init ==
+    @PostConstruct
     @Override
     public void reset() {
         smallest = 0;
@@ -31,6 +31,15 @@ public class GameImpl implements Game {
         biggest = numberGenerator.getMaxNumber();
         number = numberGenerator.next();
         log.debug("the number is {}", number);
+    }
+    @PreDestroy
+    public void preDestroy() {
+     log.info("in Game preDestroy()");
+    }
+
+   // == public methods ==
+    public void setNumberGenerator(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
     }
 
     @Override
@@ -61,8 +70,8 @@ public class GameImpl implements Game {
     @Override
     public int getRemainingGuesses() {
         return remainingGuesses;
-    }
 
+    }
 
     @Override
     public void check() {
